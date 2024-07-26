bb99b24be5e6c176e1a09ec5245ae3eb596c3a21
WebCONuz <muxammadi0799@gmail.com>
2024-07-26 10:06:41 +0500

    Completed view of resume for make pdf

--- src/views/users/MakeCvView.vue
+++ src/views/users/MakeCvView.vue
@@ -36,11 +36,11 @@ const download = () => {
         </div>
       </div>
 
-      <section class="py-6 main-shadow rounded-md">
-        <div ref="page" class="cv-content px-8">
+      <section class="w-[900px] pt-8 pb-10 main-shadow rounded-md mx-auto">
+        <div ref="page" class="cv-content px-10">
           <!-- title -->
-          <h2 class="text-2xl font-semibold mb-4">
-            Mukhammadi Toshtemirov | Frontend
+          <h2 class="text-3xl font-semibold mb-6">
+            Mukhammadi Toshtemirov | Fullstack
           </h2>
 
           <!-- socials -->
@@ -87,21 +87,167 @@ const download = () => {
             </ul>
           </div>
 
-          <!-- body -->
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, odit
-            obcaecati molestias maiores ad delectus perferendis iusto temporibus
-            sequi illo! Odit quasi architecto aliquam id delectus, pariatur
-            similique labore itaque accusantium ratione, dolore deleniti?
-            Suscipit quam cum blanditiis praesentium distinctio incidunt maxime!
-            Velit dicta expedita numquam incidunt, esse minus, ad nam nulla
-            totam fugit consequatur rem mollitia, libero animi aspernatur
-            obcaecati ea explicabo. Adipisci, corrupti atque iste magni nulla
-            amet, consequuntur dolores hic eius enim optio commodi, omnis
-            reprehenderit aliquid dolorem saepe blanditiis totam unde
-            consequatur? Adipisci atque nam molestiae sapiente, facere ullam
-            sequi temporibus, illum nisi, ab saepe possimus!
-          </p>
+          <!-- experience -->
+          <h3 class="text-lg mb-2">
+            <b>WORK EXPERIENCE</b>
+          </h3>
+          <!-- 1 -->
+          <div class="experience mb-4">
+            <div class="flex items-center justify-between">
+              <h4 class="text-lg text-gray-600">
+                <b>Najot ta'lim, Tashkent | Frontend teacher & freelancer</b>
+              </h4>
+              <b>
+                <span>Sep 2023</span> -
+                <span>Present</span>
+              </b>
+            </div>
+            <div class="pl-8">
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Based on my knowledge,
+                students were taught Vue technology
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> An e-commerce site
+                designed specifically for pets has been created. Currently in
+                the process of integrating with click, payme and grape payment
+                systems
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> A site was created for
+                the sale of cars based on the order
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Increased handling of
+                reusable components, application errors, and dynamic routing
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> I am currently building
+                my own platform for online teaching
+              </p>
+            </div>
+          </div>
+          <!-- 2 -->
+          <div class="experience mb-4">
+            <div class="flex items-center justify-between">
+              <h4 class="text-lg text-gray-600">
+                <b>Viento Group, Tashkent | Frontend developer</b>
+              </h4>
+              <b>
+                <span>May 2023</span> -
+                <span>Dec 2023</span>
+              </b>
+            </div>
+            <div class="pl-8">
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> New technologies used by
+                the team in projects were studied.
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Implemented changes to 3
+                services in production using Nuxt, vue 3, typescript, REST API
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Developed the frontend
+                part of several websites: miniature, essi, pharmlive
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Contributed to code
+                quality by reviewing other teammates' code per week
+              </p>
+            </div>
+          </div>
+
+          <!-- education -->
+          <h3 class="text-lg mb-2">
+            <b>EDUCATION</b>
+          </h3>
+          <div class="education mb-4">
+            <div class="pl-8">
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> Tashkent University of
+                Information Technologies, 2018-2022, Software engineering. GPA:
+                4.16. Level: Bachelor
+              </p>
+              <p class="text-gray-500 flex items-start">
+                <span class="text-black mr-2">-</span> "Najot Ta'lim" learning
+                center 2022-2023, Node.js+Vue.js boot camp course
+              </p>
+            </div>
+          </div>
+
+          <!-- skills -->
+          <h3 class="text-lg mb-2">
+            <b>SKILLS</b>
+          </h3>
+          <div class="skills mb-4">
+            <div class="pl-8">
+              <p class="text-gray-500 flex">
+                <span class="text-black mr-2">-</span>
+                <b class="text-gray-700 mr-2">Programming Languages:</b>
+                HTML, CSS, JavaScript, Typescript, PostgreSQL
+              </p>
+              <p class="text-gray-500 flex">
+                <span class="text-black mr-2">-</span>
+                <span>
+                  <b class="text-gray-700 mr-2">Technologies and Frameworks:</b>
+                  Node, Express, Nest, Swager, Git, Postman, Bootstap. Tailwind,
+                  Vite, React, Vue, Vuex, Pinia, Nuxt3
+                </span>
+              </p>
+              <p class="text-gray-500 flex">
+                <span class="text-black mr-2">-</span>
+                <b class="text-gray-700 mr-2">Language:</b>
+                Uzbek (native), English (intermediate)
+              </p>
+            </div>
+          </div>
+
+          <!-- reference -->
+          <h3 class="text-lg mb-2">
+            <b>REFERENCE</b>
+          </h3>
+          <div class="skills">
+            <div class="pl-8">
+              <p class="flex">
+                <span class="mr-2">-</span>
+                <span>
+                  <b class="text-gray-700 mr-2">My portfolio:</b>
+                  <router-link to="#" class="text-gray-500">
+                    toshtemirov.netlify.app
+                  </router-link>
+                </span>
+              </p>
+              <p class="flex">
+                <span class="mr-2">-</span>
+                <span>
+                  <b class="text-gray-700 mr-2">Github:</b>
+                  <router-link to="#" class="text-gray-500">
+                    github.com/WebCONuz
+                  </router-link>
+                </span>
+              </p>
+              <p class="flex">
+                <span class="mr-2">-</span>
+                <span>
+                  <b class="text-gray-700 mr-2">Some real projects:</b>
+                  <router-link to="#" class="text-gray-500">
+                    k-w, pharmlive, homedev, miniature-uz, lib.samtuit,
+                    zaminavto
+                  </router-link>
+                </span>
+              </p>
+              <p class="flex">
+                <span class="mr-2">-</span>
+                <span>
+                  <b class="text-gray-700 mr-2">Resonal Projects:</b>
+                  <router-link to="#" class="text-gray-500">
+                    My Eduon, BookApp online Servise
+                  </router-link>
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
